6ea808b392237c73445ef547e55af1a0cf154a83
janu47537 <janu47537@gmail.com>
2024-12-02 14:02:26 +0530

    Add files via upload

---
+++ b.java
@@ -0,0 +1,11 @@
+import java.io.PrintWriter;
+public class b{
+    public static void main(String[] args)
+    {
+        PrintWriter pw=new PrintWriter(System.out);
+        pw.println("Hello,World!");
+        pw.print("This is a");
+        pw.print("print statement.");
+        pw.close();
+    }
+}
