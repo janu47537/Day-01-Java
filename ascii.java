2c02572e0b1c998ea5681ce883edd1886c5b027b
janu47537 <janu47537@gmail.com>
2024-12-02 15:02:53 +0530

    Add files via upload

---
+++ ascii.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ascii
+{
+    public static void main(String[]args)
+    {
+    //conversion of ascill value to character
+    int a=50;
+    char b=(char)(a);
+    char c='7';
+    System.out.println(b);
+    System.out.println(c);
+}
+}
